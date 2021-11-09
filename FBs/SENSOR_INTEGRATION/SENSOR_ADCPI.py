30b23120cfa8ab68c0c7648ff5a21edcb0a827a0
DiogoOliveiraTA <61006042+DiogoOliveiraFEUP@users.noreply.github.com>
2021-11-09 15:11:06 +0000

    Sensor ADCPi - Correction
    Returns MEASUREMENTS + TIMESTAMPS

--- FBs/SENSOR_INTEGRATION/SENSOR_ADCPI.py
+++ FBs/SENSOR_INTEGRATION/SENSOR_ADCPI.py
@@ -1,40 +1,59 @@
 import time
+import pandas as pd
 from ADCPi import ADCPi
+
 
 class SENSOR_ADCPI:
 
     def __init__(self):
         self.adc = None
         self.measurements_list=[]
+        self.timestamps_list=[]
         self.size = 1
         self.pin = 1
 
-    def schedule(self, event_name, event_value, T_CYCLE, PIN, SIZE):
-
+    def schedule(self, event_name, event_value, PIN, SIZE):
+        
+        ### INIT EVENT ###
         if event_name == 'INIT':
-            self.adc = ADCPi(0x68, 0x69, 18)
+    
             if SIZE is None or SIZE < 1:
-                return [None, None, None, None]
+                return [None, None, None, None, None]
             if PIN is None or PIN < 1:
-                return [None, None, None, None]
+                return [None, None, None, None, None]
+    
+            self.adc = ADCPi(0x68, 0x69, 12)
             self.size = SIZE
             self.pin = PIN
-            print("ADCPi init")
-            return [None, event_value, None, None, None]
-
+            
+            #print("ADCPi init")
+    
+            return [event_value, None, None, None, None]
+        
+        ### READ EVENT ###
         elif event_name == 'READ':
-            print("ADCPi read")
+            
+            #print("ADCPi read")
+            
             i = 0
             value = 0
             while i < self.size:
                 value = self.adc.read_voltage(self.pin)
                 self.measurements_list.append(value)
-                print(value)
-                time.sleep(0.1)
+                self.timestamps_list.append(pd.Timestamp.now())
                 i+=1
-
-            value = [str(x) for x in self.measurements_list]
-            print(value)
+            
+            i = 0
+            value = ""
+            times = ""
+            while i < self.size-1:
+                value = value + str(self.measurements_list[i]) + ";"
+                times = times + str(self.timestamps_list[i]) + ";"
+                i += 1
+            value = value + str(self.measurements_list[self.size-1])
+            times = times + str(self.timestamps_list[self.size-1])            
+            
             self.measurements_list = []
-            return [None, event_value, value]
-
+            self.timestamps_list = []
+            
+            return [None, event_value, value, times]
