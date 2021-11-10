d253f04d4d4ca02bff2457c658afcff781825c00
the-grandson <lcsmneto@gmail.com>
2021-11-10 13:21:30 +0000

    Update POSTGRE_DB_INSERT.py

--- FBs/DB/POSTGRE_DB_INSERT.py
+++ FBs/DB/POSTGRE_DB_INSERT.py
@@ -1,11 +1,46 @@
-
-
+"""
 # ATENTION! First install dependencies (example for Linux): 
 # sudo apt install python3-dev libpq-dev
 # pip install psycopg2
 
 # Error handling took from: https://kb.objectrocket.com/postgresql/python-error-handling-with-the-psycopg2-postgresql-adapter-645
 # Error handling must be improved, not working with queries
+
+This block inserts the given values in the given table.
+If you fill the return values it will return also the specified return values.
+
+Consider the following table structure for the 3 examples below.
+reference_id (integer auto-increment pk) | sensor_id (integer) | actuator_id (integer)
+
+1 - Example to insert without specifying the column names, no returning values.
+This example will auto increment reference_id column.
+
+Querry:
+INSERT INTO reference_values VALUES (1,1,3)
+Block variables:
+table_name = 'reference_values'
+values = '1,1,3'
+
+
+2 - Example to insert specifying the column names, no returning values:
+This example will auto increment reference_id column.
+
+Querry:
+INSERT INTO reference_values (sensor_id,actuator_id) VALUES (1,3)
+Block variables:
+table_name = 'reference_values (sensor_id,actuator_id)'
+values = '1,1,3'
+
+3 - Example to insert specifying the column names, no returning values:
+This example will auto increment reference_id column.
+
+Querry:
+INSERT INTO reference_values (sensor_id,actuator_id) VALUES (1,3) RETURNING reference_values.id AS reference_id
+Block variables:
+table_name = 'reference_values (sensor_id,actuator_id)'
+values = '1,1,3'
+return_values = 'RETURNING reference_values.id AS reference_id'
+"""
 
 import psycopg2
 from psycopg2 import OperationalError, errorcodes, errors
@@ -20,18 +55,17 @@ class POSTGRE_DB_INSERT:
         
     def schedule(self, event_name, event_value,
                  host, port, user, password, dbname,
-                 table_name, values):
+                 table_name, values, return_value):
 
-        if event_name == 'INIT':    
-            
+        if event_name == 'INIT':
             # catch exception for invalid SQL connection
             try:
                 # declare a new PostgreSQL connection object
                 self.conn = psycopg2.connect(dbname=dbname, 
                     user=user, 
                     password=password,
-                    host="localhost",
-                    port="5432")
+                    host=host,
+                    port=port)
                 self.cursor = self.conn.cursor()
 
             except OperationalError as err:
