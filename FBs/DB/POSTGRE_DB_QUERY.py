73a04582cad94928875049d66ec62b9ab2ef999e
the-grandson <lcsmneto@gmail.com>
2023-02-16 14:57:22 +0000

    General Postgres Query FB

--- FBs/DB/POSTGRE_DB_QUERY.py
+++ FBs/DB/POSTGRE_DB_QUERY.py
@@ -6,47 +6,30 @@
 # Error handling took from: https://kb.objectrocket.com/postgresql/python-error-handling-with-the-psycopg2-postgresql-adapter-645
 # Error handling must be improved, not working with queries
 
-This block inserts the given values in the given table.
-If you fill the return values it will return also the specified return values.
+This block executes the SQL query given to the QUERY variable.
 
 Consider the following table structure for the 3 examples below.
 reference_id (integer auto-increment pk) | sensor_id (integer) | actuator_id (integer)
 
-1 - Example to insert without specifying the column names, no returning values.
+1 - Example to insert without specifying the column names.
 This example will auto increment reference_id column.
 
-Query:
+QUERY:
 INSERT INTO reference_values VALUES (1,1,3)
-Block variables:
-table_name = 'reference_values'
-insert_values = '1,1,3'
 
 
 2 - Example to insert specifying the column names, no returning values:
 This example will auto increment reference_id column.
 
-Query:
+QUERY:
 INSERT INTO reference_values (sensor_id,actuator_id) VALUES (1,3)
-Block variables:
-table_name = 'reference_values (sensor_id,actuator_id)'
-insert_values = '1,1,3'
-
-3 - Example to insert specifying the column names, with returning values:
-This example will auto increment reference_id column.
-
-Query:
-INSERT INTO reference_values (sensor_id,actuator_id) VALUES (1,3) RETURNING reference_values.id AS reference_id
-Block variables:
-table_name = 'reference_values (sensor_id,actuator_id)'
-insert_values = '1,1,3'
-return_values = 'RETURNING reference_values.id AS reference_id'
 """
 
 import psycopg2
 from psycopg2 import OperationalError, errorcodes, errors
 import json
 
-class POSTGRE_DB_INSERT:
+class POSTGRE_DB_QUERY:
 
     def __init__(self):
         self.conn = None
@@ -54,7 +37,7 @@ class POSTGRE_DB_INSERT:
         
     def schedule(self, event_name, event_value,
                  host, port, user, password, dbname,
-                 table_name, insert_values, return_values):
+                 query):
 
         if event_name == 'INIT':
             # catch exception for invalid SQL connection
@@ -69,7 +52,6 @@ class POSTGRE_DB_INSERT:
 
             except OperationalError as err:
                 print(err)
-
                 # set the connection to 'None' in case of error
                 self.conn = None
 
@@ -77,33 +59,25 @@ class POSTGRE_DB_INSERT:
                 return [event_value, None, None]
 
         elif event_name == 'RUN':
+            result = None
             if self.conn != None:
-                # insert_values must be a string with the exact number of values the table has
-                # insert_values must be separated by "," and appropriately formated, do not forget to escape \" strings         
-                query = """INSERT INTO {0} VALUES ({1}) {2};""".format(table_name,insert_values,return_values)
-                print(query)
                 
                 # catch exception for invalid SQL statement#
                 result = None
                 try:
                     self.cursor.execute(query)
                     self.conn.commit()
-
-                    # if there are values to return
-                    if return_values != None:
-                        rows = self.cursor.fetchall()
-                        if(len(rows)>0):
-                            result = json.dumps(rows)
+                    result = self.cursor.fetchall()
 
                 except Exception as err:
                     print(err)
                     # rollback the previous transaction before starting another
                     self.conn.rollback()
+                    result = str(err)
 
                 finally:
-                    # self.cursor.close()
-                    # self.conn.close()
                     return [None, event_value, result]
             else:
-                print("No active connection to PostgreSQL DB.")
-                return [event_value, None, None]
+                result = "No active connection to PostgreSQL DB."
+                print(result)
+                return [event_value, None, result]
