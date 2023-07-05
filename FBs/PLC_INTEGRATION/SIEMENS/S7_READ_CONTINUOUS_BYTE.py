ab38d1555e3c59ecc8b0252cdd87aedf92766749
João Pinheiro <jmotp@live.com.pt>
2023-07-05 11:49:15 +0100

    Handle exceptions S7_READ

--- FBs/PLC_INTEGRATION/SIEMENS/S7_READ_CONTINUOUS_BYTE.py
+++ FBs/PLC_INTEGRATION/SIEMENS/S7_READ_CONTINUOUS_BYTE.py
@@ -14,16 +14,22 @@ class S7_READ_CONTINUOUS_BYTE:
     def schedule(self, event_name, event_value, ip_address, rack, number, port, db_number,start, bit):
         
         if event_name == 'INIT':
-            self.client.connect(ip_address,rack,number,port)
-            status = self.client.get_connected()
+            try:
+                self.client.connect(ip_address,rack,number,port)
+                status = self.client.get_connected()
+            except:
+                status = False
             if(status == True):
                 print('Connection sucessfully initiated with device at {0}'.format(ip_address))
             else:
                 print('Error initiating connection with S7 PLC')
             return [event_value, None, None, None, None, None, None, None, None ,None, 0,self.bits[0], self.bits[1], self.bits[2], self.bits[3], self.bits[4], self.bits[5], self.bits[6], self.bits[7]]
 
-        elif event_name == 'READ':            
-            data = self.client.db_read(db_number,start,1)
+        elif event_name == 'READ':
+            try:            
+                data = self.client.db_read(db_number,start,1)
+            except:
+                data = self.old_value
             int_val = int.from_bytes(data, "big")
             #calculate edge transitions
             changes = int_val ^ self.old_value
